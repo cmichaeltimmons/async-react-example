3e5d30776447f7816d89156725c1fcb64952e9b9
Michael Timmons <michael.timmons@protonmail.com>
2023-01-06 20:22:14 -0500

    Init demo

---
+++ api.js
@@ -0,0 +1,90 @@
+const transactions = [
+    {
+        name: 'Ball',
+        price: 110,
+        date: '2020-01-01',
+        customerId: 1
+    },
+    {
+        name: 'Shoes',
+        price: 20,
+        date: '2020-01-15',
+        customerId: 3
+    },
+    {
+        name: 'Shirt',
+        price: 70,
+        date: '2020-01-16',
+        customerId: 2
+    },
+    {
+        name: 'Pants',
+        price: 100,
+        date: '2020-02-03',
+        customerId: 2
+    },
+    {
+        name: 'Hat',
+        price: 40,
+        date: '2020-02-15',
+        customerId: 2
+    },
+    {
+        name: 'Socks',
+        price: 80,
+        date: '2020-03-01',
+        customerId: 3
+    },
+    {
+        name: 'Gloves',
+        price: 70,
+        date: '2020-03-20',
+        customerId: 3
+    },
+    {
+        name: 'Sweater',
+        price: 100,
+        date: '2020-03-25',
+        customerId: 1
+    }
+]
+
+const convertToPoints = (price) => {
+    if (price > 100) {
+        return (price - 100) * 2 + 50
+    } else if (price > 50) {
+        return price - 50
+    } else {
+        return 0
+    }
+}
+
+const calculateRewardPoints = (transactions) => {
+    const customers = {}
+    const months = new Set()
+    transactions.forEach(transaction => {
+        const { customerId, price, date } = transaction
+        const month = date.split('-')[1]
+        months.add(month)
+        const customer = customers[customerId] || {}
+        const monthlyPoints = customer[month] || 0
+        const points = convertToPoints(price)
+        customer[month] = monthlyPoints + points
+        customer['total'] = (customer['total'] || 0) + points
+        customers[customerId] = customer
+    })
+
+    return { customers, months: Array.from(months), transactions }
+}
+
+
+// fake aync  time out one second api for get rewards 
+export const api = {
+    getData: () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(calculateRewardPoints(transactions))
+            }, 1000)
+        })
+    }
+}
---
+++ index.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { api } from './api.js';
+
+const Transactions = props => {
+    const { data } = props
+    const { transactions } = data
+    return (
+        <div>
+            <h1>Transactions</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Customer Id</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map((transaction, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{transaction.customerId}</td>
+                                <td>{transaction.name}</td>
+                                <td>{transaction.price}</td>
+                                <td>{transaction.date}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+const months = {
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec'
+}
+
+const Results = props => {
+    const { data } = props
+    return (
+        <div>
+            <h1>Reward Points</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Customer Id</th>
+                        {Array.from(data.months).map(month => {
+                            return <th>{months[month]}</th>
+                        })}
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(data.customers).map((customerId, index) => {
+                        const customer = data.customers[customerId]
+                        return (
+                            <tr key={index}>
+                                <td>{customerId}</td>
+                                {Array.from(data.months).map(month => {
+                                    return <td>{customer[month] || 0}</td>
+                                })}
+                                <td>{customer['total'] || 0}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+const App = () => {
+    const [data, setData] = useState({ customers: {}, transactions: [], months: [] })
+    useEffect(() => {
+        async function getData() {
+            const transactions = await api.getData()
+            setData(transactions)
+        }
+
+        getData()
+    }, [])
+
+    if (data.months.length === 0) return (<div>Loading...</div>)
+
+    return (
+        <div>
+            <Transactions data={data} />
+            <Results data={data} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
